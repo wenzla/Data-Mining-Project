864ceb141d08ec8c757f566b743f58e1617780f1
kevin truong <kevintruong@kevins-MacBook-Pro.local>
2018-03-23 18:00:30 -0400

    Classfying with KNN

--- FeatureVectorsTitleCount.py
+++ FeatureVectorsTitleCount.py
@@ -2,7 +2,8 @@
 import csv
 import re
 import math
-
+from string import digits
+import random
 
 count = list()
 topic = list()
@@ -32,23 +33,27 @@ for i in range(0, len(topic)):
     tempArray.append(output)
 
 #prints the whole feature vector to screen
+print "Start of feature vector: "
 for i in range(0, len(countVector)):
     print countVector[i]
+print "Finshed printing feature vector..."
 
 #gets first 1900 for training classifer and the last 100 for data
 xyPoints = tempArray[:testsplit]
 xPoint = list()
 yPoint = list()
-
+topic = list()
+rp = []
 #classifier data set
 pointsOfLast = tempArray[testsplit:]
-
 
 #Save points of first 1900
 for i in range(0, len(xyPoints)):
     line = xyPoints[i].split(" ")
+    rp.append(tuple([line[0],line[1]]))
     xPoint.append(line[0])
     yPoint.append(line[1])
+    topic.append(line[2])
 
 #points for last 100
 classxPoint = list()
@@ -59,27 +64,42 @@ for i in range(0, len(pointsOfLast)):
     classxPoint.append(line[0])
     classyPoint.append(line[1])
 
+
+ResultTopics = []
+xy = list()
 EuclideanDistance = []
+result = []
+print "printing distances...."
 for x in range(0, len(pointsOfLast)):
     #cordnates for thing that needs to be classfied
     X1 = float(classxPoint[x])
     Y1 = float(classyPoint[x])
-    TempEuclideanDistance = []
+    TempEuclideanDistance = [100]
+    ty = []
+    tx = []
 
     for i in range(0, len(xyPoints)):
         #cordnates for known
         X2 = float(xPoint[i])
         Y2 = float(yPoint[i])
-
+        point = tuple([0,0])
         distance = math.sqrt((X1 - X2)**2 + (Y1- Y2)**2)
         print distance
-        TempEuclideanDistance.append(distance)
+        #Gets the shortest distance
+        if (distance > 1 and (distance < max(TempEuclideanDistance))):
+            TempEuclideanDistance.append(distance)
+            point = tuple([str(X2),str(Y2)])
 
-    #saves the shortest distance to all
+#loops through all topics and matches up data point to
+    for a in range(0, len(rp)):
+        if point == rp[a]:
+            resultingTopc = topics[a]
+            result.append(resultingTopc)
 
-    EuclideanDistance.append(min(TempEuclideanDistance))
-    #get min then route it back to the topic of known dataset
+        #get the topic based on that point, this is the topic of theu unknown
+    #print TempEuclideanDistance
+print "printing result"
+print result
 
-print len(EuclideanDistance)
 
-#after routing back to topics, need to figure out accuracy
+#opens the avliable topics loop through all and get the topic based on the closest data point
